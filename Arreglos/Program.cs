c17a96fe187cda2756dc367a3d4dacc7da2f34f6
Abraham Araujo Moreno <alex_91y@hotmail.com>
2016-12-01 00:17:11 -0600

    Add files via upload

---
+++ Arreglos/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arreglos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arreglo = new int[3]; //arreglo de tres elementos
+
+            arreglo[0] = 0;
+            arreglo[1] = 2;
+            arreglo[2] = 4;
+
+            Console.WriteLine(arreglo[0]);
+            Console.ReadKey();
+        }
+    }
+}
